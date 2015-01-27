6edb05007f9989ab4018d1d87b1c13832699d0fb
omissis <claudi0.beatric3@gmail.com>
2015-01-27 02:06:17 +0100

    Removed test conditional in StringEqualsDataMatcherTest.

--- tests/src/Unit/DataMatcher/StringEqualsDataMatcherTest.php
+++ tests/src/Unit/DataMatcher/StringEqualsDataMatcherTest.php
@@ -15,38 +15,105 @@ use Drupal\Tests\rules\Unit\RulesUnitTestBase;
  * @group rules
  */
 class StringEqualsDataMatcherTest extends RulesUnitTestBase {
+
   /**
-   * @dataProvider matchesProvider
+   * @dataProvider caseSensitiveTrimmedMatchesProvider
    */
-  public function testMatch($expectedMatchResult, $trim, $case_sensitive, $subject, $object) {
+  public function testCaseSensitiveTrimmedMatch($expectedMatchResult, $subject, $object) {
     $matcher = StringEqualsDataMatcher::create();
 
-    if ($case_sensitive) {
-      $matcher->setCaseSensitive();
-    } else {
-      $matcher->setCaseInsensitive();
-    }
+    $matcher->setCaseSensitive();
 
-    if ($trim) {
-      $matcher->setTrimmed();
-    } else {
-      $matcher->unsetTrimmed();
-    }
+    $matcher->setTrimmed();
 
     $this->assertSame($expectedMatchResult, $matcher->match($subject, $object));
   }
 
-  public function matchesProvider() {
-    return array(
-      array(TRUE, TRUE, TRUE, 'foo ', ' foo'),
-      array(TRUE, FALSE, TRUE, 'foo', 'foo'),
-      array(TRUE, TRUE, FALSE, 'foo ', ' FOO'),
-      array(TRUE, FALSE, FALSE, 'foo', 'foo'),
+  /**
+   * @dataProvider caseInsensitiveTrimmedMatchesProvider
+   */
+  public function testCaseInsensitiveTrimmedMatch($expectedMatchResult, $subject, $object) {
+    $matcher = StringEqualsDataMatcher::create();
 
-      array(FALSE, TRUE, TRUE, 'foo ', ' fo'),
-      array(FALSE, FALSE, TRUE, 'foo', 'FOO'),
-      array(FALSE, TRUE, FALSE, 'foo ', ' FO'),
-      array(FALSE, FALSE, FALSE, 'foo', 'fo'),
+    $matcher->setCaseInsensitive();
+
+    $matcher->setTrimmed();
+
+    $this->assertSame($expectedMatchResult, $matcher->match($subject, $object));
+  }
+
+  /**
+   * @dataProvider caseSensitiveUntrimmedMatchesProvider
+   */
+  public function testCaseSensitiveUntrimmedMatch($expectedMatchResult, $subject, $object) {
+    $matcher = StringEqualsDataMatcher::create();
+
+    $matcher->setCaseSensitive();
+
+    $matcher->unsetTrimmed();
+
+    $this->assertSame($expectedMatchResult, $matcher->match($subject, $object));
+  }
+
+  /**
+   * @dataProvider caseInsensitiveUntrimmedMatchesProvider
+   */
+  public function testCaseInsensitiveUntrimmedMatch($expectedMatchResult, $subject, $object) {
+    $matcher = StringEqualsDataMatcher::create();
+
+    $matcher->setCaseInsensitive();
+
+    $matcher->unsetTrimmed();
+
+    $this->assertSame($expectedMatchResult, $matcher->match($subject, $object));
+  }
+
+  public function caseSensitiveTrimmedMatchesProvider() {
+    return array(
+      array(TRUE,  'foo ', ' foo'),
+      array(TRUE,  'foo',  'foo'),
+      array(FALSE, 'foo ', ' FOO'),
+      array(FALSE, 'foo ', ' fo'),
+      array(FALSE, 'foo',  'FOO'),
+      array(FALSE, 'foo ', ' FO'),
+      array(FALSE, 'foo',  'fo'),
     );
   }
+
+  public function caseInsensitiveTrimmedMatchesProvider() {
+    return array(
+      array(TRUE,  'foo ', ' foo'),
+      array(TRUE,  'foo',  'foo'),
+      array(TRUE,  'foo ', ' FOO'),
+      array(FALSE, 'foo ', ' fo'),
+      array(TRUE,  'foo',  'FOO'),
+      array(FALSE, 'foo ', ' FO'),
+      array(FALSE, 'foo',  'fo'),
+    );
+  }
+
+  public function caseSensitiveUntrimmedMatchesProvider() {
+    return array(
+      array(FALSE, 'foo ', ' foo'),
+      array(TRUE,  'foo',  'foo'),
+      array(FALSE, 'foo ', ' FOO'),
+      array(FALSE, 'foo ', ' fo'),
+      array(FALSE, 'foo',  'FOO'),
+      array(FALSE, 'foo ', ' FO'),
+      array(FALSE, 'foo',  'fo'),
+    );
+  }
+
+  public function caseInsensitiveUntrimmedMatchesProvider() {
+    return array(
+      array(FALSE,  'foo ', ' foo'),
+      array(TRUE,  'foo',  'foo'),
+      array(FALSE,  'foo ', ' FOO'),
+      array(FALSE, 'foo ', ' fo'),
+      array(TRUE,  'foo',  'FOO'),
+      array(FALSE, 'foo ', ' FO'),
+      array(FALSE, 'foo',  'fo'),
+    );
+  }
+
 }
